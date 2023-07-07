f2f16708884833f7efb849e91ccc7cea65c8cb6d
Juan Camilo <lopezquekk@gmail.com>
2023-07-06 20:20:19 -0500

    add documentation

--- Sources/LPReducer/LPReducer.swift
+++ Sources/LPReducer/LPReducer.swift
@@ -3,11 +3,12 @@ import SwiftUI
 import Combine
 
 @dynamicMemberLookup
-final class Store<R: Reducer>: ObservableObject {
+/// The dynamic store, is a store which contains the `state`, the `reducer` and the `actions`
+public final class Store<R: Reducer>: ObservableObject {
     let reducer: R = R.init()
     private var cancellables: [AnyHashable: AnyCancellable] = [:]
     private var taskCancellables: [AnyHashable: Task<Void, Never>] = [:]
-    private(set) lazy var objectWillChange = ObservableObjectPublisher()
+    
     private var provisionalState: R.State
     private(set) var state: R.State {
         didSet {
@@ -32,7 +33,7 @@ final class Store<R: Reducer>: ObservableObject {
     }
     
     @MainActor
-    func binding<T>(_ keyPath: WritableKeyPath<R.State, T>) -> Binding<T> {
+    public func binding<T>(_ keyPath: WritableKeyPath<R.State, T>) -> Binding<T> {
         Binding {
             self.provisionalState[keyPath: keyPath]
         } set: {
@@ -42,7 +43,7 @@ final class Store<R: Reducer>: ObservableObject {
     }
     
     @MainActor
-    func binding<T>(_ keyPath: WritableKeyPath<R.State, T>, action: R.Action) -> Binding<T> {
+    public func binding<T>(_ keyPath: WritableKeyPath<R.State, T>, action: R.Action) -> Binding<T> {
         Binding {
             self.provisionalState[keyPath: keyPath]
         } set: {
@@ -65,31 +66,31 @@ final class Store<R: Reducer>: ObservableObject {
     @MainActor
     private func resolveOperation(_ operation: Operation<R.Action>) async {
         switch operation {
-        /// `.asyncSequence`
-        /// A way for use asyncSequence based on a AsyncStream
-        /// ```swift
-        ///     return .asyncSequence(
-        ///           stream: monitor.paths(),
-        ///           cancellableId: "cancellableId") { result in
-        ///               if let path = result as? NWPath {
-        ///                  print(path.status)
-        ///               }
-        ///               await .printStatus(true)
-        ///     }```
-        ///  For using this case you'll need to create a asyncStream using
-        ///  `AsyncStream<Sendable>`
-        ///  ```swift
-        ///     func paths() -> AsyncStream<Sendable> {
-        ///         AsyncStream { continuation in
-        ///             pathUpdateHandler = { path in
-        ///             continuation.yield(path)
-        ///         }
-        ///         continuation.onTermination = { [weak self] _ in
-        ///             self?.cancel()
-        ///         }
-        ///         start(queue: DispatchQueue(label: "NSPathMonitor.paths"))
-        ///     }```
-        ///  you can cancel it anytime by using `.cancel(cancellableId)`
+            /// `.asyncSequence`
+            /// A way for use asyncSequence based on a AsyncStream
+            /// ```swift
+            ///     return .asyncSequence(
+            ///           stream: monitor.paths(),
+            ///           cancellableId: "cancellableId") { result in
+            ///               if let path = result as? NWPath {
+            ///                  print(path.status)
+            ///               }
+            ///               await .printStatus(true)
+            ///     }```
+            ///  For using this case you'll need to create a asyncStream using
+            ///  `AsyncStream<Sendable>`
+            ///  ```swift
+            ///     func paths() -> AsyncStream<Sendable> {
+            ///         AsyncStream { continuation in
+            ///             pathUpdateHandler = { path in
+            ///             continuation.yield(path)
+            ///         }
+            ///         continuation.onTermination = { [weak self] _ in
+            ///             self?.cancel()
+            ///         }
+            ///         start(queue: DispatchQueue(label: "NSPathMonitor.paths"))
+            ///     }```
+            ///  you can cancel it anytime by using `.cancel(cancellableId)`
         case let .asyncSequence(sequence, cancellableId, toExecute):
             let iterator = Task {
                 for await res in sequence {
@@ -98,31 +99,31 @@ final class Store<R: Reducer>: ObservableObject {
             }
             
             taskCancellables[cancellableId] = iterator
-        /// This represent not additional action to perform
+            /// This represent not additional action to perform
         case .none:
             break
-        /// This case represent a background action that you want to perform in background like
-        /// calling a service from the internet, this is suppose to be executed outside of the
-        /// `MainActor` scope.
-        /// Once this background process finishes it will trigger another action back on the `MainActor` context
+            /// This case represent a background action that you want to perform in background like
+            /// calling a service from the internet, this is suppose to be executed outside of the
+            /// `MainActor` scope.
+            /// Once this background process finishes it will trigger another action back on the `MainActor` context
         case let .task(nextAction):
             Task.detached(priority: .background) {
                 await self.send(nextAction())
             }
-        /// this work exactly the same way as `.task(nextAction)`
-        /// but without trigger any other action
+            /// this work exactly the same way as `.task(nextAction)`
+            /// but without trigger any other action
         case let .run(toExecute):
             Task.detached(priority: .background) {
                 await toExecute()
             }
-        /// This one wil trigger an new action in the `MainActor` context, recommended for synchronous processes
+            /// This one wil trigger an new action in the `MainActor` context, recommended for synchronous processes
         case let .action(act):
             send(act)
-        /// You can send two `Operations` for execute both one after the other
+            /// You can send two `Operations` for execute both one after the other
         case let .syncMerge(act1, act2):
             await resolveOperation(act1)
             await resolveOperation(act2)
-        /// You can send two `Operations` concurrently
+            /// You can send two `Operations` concurrently
         case let .asyncMerge(act1, act2):
             await withTaskGroup(of: Void.self) { taskGroup in
                 taskGroup.addTask {
@@ -133,17 +134,17 @@ final class Store<R: Reducer>: ObservableObject {
                     await self.resolveOperation(act2)
                 }
             }
-        /// This case will create a `Timer.publish`and you can execute a action for every iteration
-        /// Cancel it any time by using: `.cancel()`
-        ///
-        /// ```swift
-        ///     return .timer(
-        ///         every: 1,
-        ///         on: .main,
-        ///         in: .default,
-        ///         cancellableId: TimerID(),
-        ///         action: .updateTime)```
-        ///
+            /// This case will create a `Timer.publish`and you can execute a action for every iteration
+            /// Cancel it any time by using: `.cancel()`
+            ///
+            /// ```swift
+            ///     return .timer(
+            ///         every: 1,
+            ///         on: .main,
+            ///         in: .default,
+            ///         cancellableId: TimerID(),
+            ///         action: .updateTime)```
+            ///
         case let .timer(interval, runLoop, mode, cancellableId, action):
             let cancellable = Timer.publish(every: interval, on: runLoop, in: mode)
                 .autoconnect()
@@ -159,16 +160,17 @@ final class Store<R: Reducer>: ObservableObject {
     }
 }
 
-protocol Reducer<State, Action> {
+public protocol Reducer<State, Action> {
     associatedtype Action
     associatedtype State: Equatable
     
     init()
     
+    /// Everytime the view send an action this function will be triggered
     @MainActor func reduce(into state: inout State, _ action: Action) -> Operation<Action>
 }
 
-indirect enum Operation<Action> {
+public indirect enum Operation<Action> {
     case asyncSequence(stream: AsyncStream<Sendable>,
                        cancellableId: AnyHashable,
                        @Sendable (Sendable) async -> Action)
